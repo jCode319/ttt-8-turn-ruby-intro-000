7d2a4c2ebb264939682de0fc8d1574809e96ff0f
Jarrell Jackson <jdjack3@gmail.com>
2020-08-18 15:56:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,9 +4,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
 
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-" * 11
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-" * 11
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end

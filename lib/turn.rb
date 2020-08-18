7afe0167c59604aec6d068d7fa233926487b3eca
Jarrell Jackson <jdjack3@gmail.com>
2020-08-18 15:47:13 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+#require_relative '..bin/turn'
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-" * 11
+  puts "   |   |   "
+  puts "-" * 11
+  puts "   |   |   "
+end
